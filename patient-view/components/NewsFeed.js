e405910f9f1a2a3da02bfca54286779776d291d7
Monica Schmiede <monicaschmiede@Monicas-MacBook-Air.local>
2022-01-28 15:07:00 -0800

    Monicas changes

--- patient-view/components/NewsFeed.js
+++ patient-view/components/NewsFeed.js
@@ -1,11 +1,14 @@
-import {Pressable, Text, View} from "react-native";
+import {Pressable, StyleSheet, Text, View} from "react-native";
 import React from "react";
 
 function NewsFeed({ navigation }) {
   return(
    <View>
      <Pressable onPress={() => navigation.goBack()}>
-       <Text>Latest Content Library</Text>
+       <Text>starting w metro bundler</Text>
+     </Pressable>
+     <Pressable onPress={() => navigation.goBack()}>
+       <Text>starting w metro bundler</Text>
      </Pressable>
      <Pressable onPress={() => navigation.goBack()}>
        <Text>type something else</Text>
@@ -13,23 +16,22 @@ function NewsFeed({ navigation }) {
      <Pressable onPress={() => navigation.goBack()}>
        <Text>type something else</Text>
      </Pressable>
-     <Pressable onPress={() => navigation.goBack()}>
+        <Pressable onPress={() => navigation.goBack()}>
        <Text>type something else</Text>
      </Pressable>
      <Pressable onPress={() => navigation.goBack()}>
        <Text>type something else</Text>
      </Pressable>
      <Pressable onPress={() => navigation.goBack()}>
-       <Text>type something else</Text>
-     </Pressable>
-     <Pressable onPress={() => navigation.goBack()}>
-       <Text>type something else</Text>
-     </Pressable>
-     <Pressable onPress={() => navigation.goBack()}>
-       <Text>type something else</Text>
+       <Text style={styles.feed}>type something else</Text>
      </Pressable>
    </View>
   )
 }
 
+const styles = StyleSheet.create({
+  feed: {
+    'color': 'blue'
+  }
+});
 export default NewsFeed;
